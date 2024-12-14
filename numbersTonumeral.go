b117b663c764e709d4e97894dafcf97692ff11a2
Balachander Rao Paalepu <balu@mac.home>
2024-12-13 21:22:33 -0500

    changes

--- numbersTonumeral.go
+++ numbersTonumeral.go
@@ -5,9 +5,8 @@ import (
 	"strings"
 )
 
-var units = []string{"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
-var tens = []string{"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"}
-var tenss = []string{"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"}
+var units = []string{"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine","ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"}
+var tens = []string{"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"}
 var numerals = []string{" hundred", " thousand", " lakh", " crore"}
 
 func main(){
@@ -45,17 +44,13 @@ func main(){
 			}
 
 	 result:=" ";
-		if n < 10 {
+		if n < 20 {
 
 			result=units[n]
 
-		} else if n < 20  {
-
-			result=tens[n-10]
-
 		} else if n < 100 { 
 
-			result=tenss[n/10] + " " + units[n%10]
+			result=tens[n/10] + " " + units[n%10]
 
 		}else if n<1000{
 
